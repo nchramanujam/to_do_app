cacc88a4187fe2436b8c23df502539d3be05a94e
nchramanujam <nch.ramanujam@gmail.com>
2026-08-26 13:59:11 -0400

    add clock and exception handleded

--- gui.py
+++ gui.py
@@ -1,14 +1,17 @@
 import functions
 import FreeSimpleGUI as sg
 import layout as l
+import time
 
-window = sg.Window("My Todo App", layout = [[l.label],
+
+window = sg.Window("My Todo App", layout = [[l.clock],[l.label],
                                             [l.input, l.add_button],
-                                            [l.list_box, l.edit_button , l.delete_button],],
+                                            [l.list_box, l.edit_button , l.delete_button], [l.exit_button]],
                    font=("Helvetica", 12))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=1000)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
     match event:
@@ -19,23 +22,31 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select item", font=("Helvetica", 12))
         case "todos":
             window['todo'].update(value = values['todos'][0])
         case "Complete":
-            todo_to_delete = values['todos'][0]
-            new_todo = values['todo']
-            todos = functions.get_todos()
-            index = todos.index(todo_to_delete)
-            todos.pop(index)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_delete = values['todos'][0]
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                index = todos.index(todo_to_delete)
+                todos.pop(index)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select item",  font=("Helvetica", 12))
+        case "Exit":
+            exit()
         case sg.WIN_CLOSED:
             break
 
--- layout.py
+++ layout.py
@@ -1,6 +1,8 @@
 import FreeSimpleGUI as sg
 import functions
 
+sg.theme("DarkPurple4")
+clock = sg.Text('', key='clock')
 label =  sg.Text("Type in Todos")
 input = sg.Input(tooltip="Enter Todo", key="todo")
 add_button = sg.Button("Add")
@@ -9,3 +11,4 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True,
                       size=[45, 10])
 delete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
