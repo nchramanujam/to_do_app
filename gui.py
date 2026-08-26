26ef0c41b71f8fa81f2629b47d630327b7c329b2
nchramanujam <nch.ramanujam@gmail.com>
2026-08-25 20:31:52 -0400

    Update complete feature

--- gui.py
+++ gui.py
@@ -2,7 +2,10 @@ import functions
 import FreeSimpleGUI as sg
 import layout as l
 
-window = sg.Window("My Todo App", layout = [[l.label], [l.input, l.add_button]], font=("Helvetica", 12))
+window = sg.Window("My Todo App", layout = [[l.label],
+                                            [l.input, l.add_button],
+                                            [l.list_box, l.edit_button , l.delete_button],],
+                   font=("Helvetica", 12))
 
 while True:
     event, values = window.read()
@@ -11,9 +14,28 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todos"] + "\n"
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value = values['todos'][0])
+        case "Complete":
+            todo_to_delete = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_delete)
+            todos.pop(index)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case sg.WIN_CLOSED:
             break
 
--- layout.py
+++ layout.py
@@ -1,5 +1,11 @@
 import FreeSimpleGUI as sg
+import functions
 
 label =  sg.Text("Type in Todos")
-input = sg.Input(tooltip="Enter Todo", key="todos")
+input = sg.Input(tooltip="Enter Todo", key="todo")
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True,
+                      size=[45, 10])
+delete_button = sg.Button("Complete")
