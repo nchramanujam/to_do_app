563474ace2a096159c88bc6625c45b7485fd2a2d
nchramanujam <nch.ramanujam@gmail.com>
2026-08-26 23:23:05 -0400

    todo web based version

--- web.py
+++ web.py
@@ -10,8 +10,13 @@ def add_todo():
 todos = functions.get_todos()
 st.title("Todos App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='',placeholder="Add new Todos", on_change=add_todo, key="add_todo")
 
